ad764c3d29b77b8da735f29362aa35f671eab7a2
yeti2391 <marco2391@gmail.com>
2025-12-18 22:41:11 -0300

    se comienza a trabajar en la view y a darle esestructura

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Mi vista de inicio
 def home(request):
-    return HttpResponse("Welcome to the Home Page!")
+    return render(request, 'core/home.html')
